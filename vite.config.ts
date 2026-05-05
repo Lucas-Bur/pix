b94e8e8c82aad4853c975f0df28e90927f369dc6
Lucas Burmeister <lucas.burmeister@gmx.de>
2026-05-05 23:03:42 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite-plus"
+
+export default defineConfig({
+  pack: {
+    entry: ["src/index.ts"],
+    format: ["esm"],
+    dts: false,
+    banner: { js: "#!/usr/bin/env node" },
+    // shims: true,
+    // exports: true,
+  },
+  test: {
+    include: ["src/**/*.test.ts"],
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {
+    semi: false,
+    singleQuote: false,
+    jsdoc: true,
+    sortImports: true,
+    sortPackageJson: true,
+    useTabs: false,
+    trailingComma: "all",
+  },
+})
